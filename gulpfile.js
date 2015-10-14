b1be26b21a513ef6fea0b05854a5e9d41aa86eb9
Jacob Zimmerman <jczimm@jczimm.com>
2015-10-13 20:15:39 -0400

    now copies app.js to app/, resolving packaging issue; gulp-debug
    * see line 31 of TODO
    * added gulp-debug logging files to be built

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp-param')(require('gulp'), process.argv),
     sourcemaps = require('gulp-sourcemaps'),
     gutil = require('gulp-util'),
+    debug = require('gulp-debug'),
     rucksack = require('gulp-rucksack'),
     autoprefixer = require('gulp-autoprefixer'),
     sass = require('gulp-sass'),
@@ -17,18 +18,19 @@ var util = require('./util');
 process.env.NODE_ENV = "development";
 
 const SRC_DIR = './src/';
+const APP_JS = ['./app.js'];
 
 // live-reloading application for development
 var electron = require('electron-connect').server.create();
 
-gulp.task('serve', function() {
-    gulp.start('build');
+gulp.task('dev', ['build', 'serve']); // serve after build is complete
 
+gulp.task('serve', function() {
     // Start browser process
     electron.start();
 
-    // Restart browser process when app.js is modified
-    gulp.watch('./app.js', electron.restart);
+    // Rebuild app.js and restart browser process when app.js is modified
+    gulp.watch(APP_JS, ["build:app-js", "reload:browser"]);
 
     // Rebuild files and reload renderer process when any change(s) are made to the page
     gulp.watch(SRC_DIR + '**/*', ["build:client", "reload:renderer"]);
@@ -50,61 +52,64 @@ gulp.task('reload:renderer', function() {
 const DEST = './app/';
 const GLOBAL_EXCLUDE = ['!' + SRC_DIR + 'lib/*'];
 
-var forceChanged = function forceChanged(stream, cb, sourceFile) {
-    // simple: always push file to stream
-    stream.push(sourceFile);
-    cb();
-};
-
-var changedOpts = function changedOpts(force) {
-    return {
-        hasChanged: (force === true ? forceChanged : changed.compareLastModifiedTime)
-    };
-};
 
 gulp.task('build', ['build:app-js', 'build:client']);
-gulp.task('build:client', ['build:js', 'build:css', 'copy-the-rest']);
+gulp.task('build:client', ['build:html', 'build:js', 'build:css', 'copy-the-rest']);
 
-const APP_JS = ['./app.js'];
 gulp.task('build:app-js', function(force) {
     return gulp.src(APP_JS.concat(GLOBAL_EXCLUDE))
-        .pipe(changed(DEST, changedOpts(force))) // only let "dirty" files through (files that have been modified since the last build)
+        .pipe(changed(DEST, util.changedOpts(force))) // only let "dirty" files through (files that have been modified since the last build)
+        .pipe(debug()) // log all files that will be built
+
+        .pipe(gulp.dest(DEST));
+});
+
+const HTML_SRC = [SRC_DIR + 'index.html'];
+gulp.task('build:html', function(force) {
+    return gulp.src(HTML_SRC.concat(GLOBAL_EXCLUDE))
+        .pipe(changed(DEST, util.changedOpts(force))) // only let "dirty" files through
+        .pipe(debug()) // log all files that will be built
+
         .pipe(gulp.dest(DEST));
 });
 
 const JS_SRC = [SRC_DIR + '**/*.js'];
 gulp.task('build:js', function(force) {
     return gulp.src(JS_SRC.concat(GLOBAL_EXCLUDE))
-        .pipe(changed(DEST, changedOpts(force))) // only let "dirty" files through
+        .pipe(changed(DEST, util.changedOpts(force))) // only let "dirty" files through
+        .pipe(debug()) // log all files that will be built
         .pipe(sourcemaps.init()) // init sourcemaps
 
-    .pipe(babel().on('error', util.logErrorWithoutColorCodes)) // transpile ES6 to ES5
+        .pipe(babel().on('error', util.logErrorWithoutColorCodes)) // transpile ES6 to ES5
 
-    .pipe(sourcemaps.write('.')) // write all the source maps
+        .pipe(sourcemaps.write('.')) // write all the source maps
 
-    .pipe(gulp.dest(DEST));
+        .pipe(gulp.dest(DEST));
 });
 
 const CSS_SRC = [SRC_DIR + '**/*.scss'];
 gulp.task('build:css', function(force) {
     return gulp.src(CSS_SRC.concat(GLOBAL_EXCLUDE))
-        .pipe(changed(DEST, changedOpts(force))) // only let "dirty" files through
+        .pipe(changed(DEST, util.changedOpts(force, '.css'))) // only let "dirty" files through
+        .pipe(debug()) // log all files that will be built
         .pipe(sourcemaps.init()) // init sourcemaps
 
-    .pipe(sass().on('error', sass.logError)) // translate the scss into css
-        .pipe(rucksack()) // process the css with rucksack (includes various CSS "superpowers")
-        .pipe(autoprefixer('last 20 versions')) // add vendor prefixes
+        .pipe(sass().on('error', sass.logError)) // translate the scss into css
+            .pipe(rucksack()) // process the css with rucksack (includes various CSS "superpowers")
+            .pipe(autoprefixer('last 20 versions')) // add vendor prefixes
 
-    .pipe(sourcemaps.write('.')) // write all the source maps
+        .pipe(sourcemaps.write('.')) // write all the source maps
 
-    .pipe(gulp.dest(DEST));
+        .pipe(gulp.dest(DEST));
 });
 
 // invert SRC paths that have already been built so that this task only copies everything else
-const MISC_SRC = util.uniq([].concat.apply([], [JS_SRC, CSS_SRC].map(util.invertGulpSrcPath))).concat([SRC_DIR + '**/*']);
+const MISC_SRC = util.uniq([].concat.apply([], [HTML_SRC, JS_SRC, CSS_SRC].map(util.invertGulpSrcPath))).concat([SRC_DIR + '**/*']);
 gulp.task('copy-the-rest', function(force) {
     return gulp.src(MISC_SRC)
-        .pipe(changed(DEST, changedOpts(force))) // only let "dirty" files through
+        .pipe(changed(DEST, util.changedOpts(force))) // only let "dirty" files through
+        .pipe(debug()) // log all files that will be built
+
         .pipe(gulp.dest(DEST));
 });
 
