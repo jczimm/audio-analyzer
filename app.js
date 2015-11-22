1207dca980d546866e1e422424baa3435a55c2fc
jczimm <jczimm.alias@gmail.com>
2015-11-21 22:58:25 -0500

    != -> !==

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ ipc.on("window", function(e, command) {
 function quitApp() {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform != 'darwin') {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 }
