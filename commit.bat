e9da20a4baffb6791de6a0a91a22d7370770a290
Jacob Zimmerman <jczimm@jczimm.com>
2016-01-04 17:55:44 -0500

    chore: Fixed run path of git-cz now to use C:\Users\Jacob\AppData\Roaming
    Carelessly I had put *my* absolute AppData/Roaming directory...

--- commit.bat
+++ commit.bat
@@ -1,2 +1,2 @@
 @echo off
-start cmd /q /k "mode con: cols=100 lines=40 && C:/Users/Jacob/AppData/Roaming/npm/git-cz && powershell -command '&{$H=get-host;$W=$H.ui.rawui;$B=$W.buffersize;$B.width=100;$B.height=9999;$W.bu‌​ffersize=$B;}'"
+start cmd /q /k "mode con: cols=100 lines=40 && %AppData%/npm/git-cz && powershell -command '&{$H=get-host;$W=$H.ui.rawui;$B=$W.buffersize;$B.width=100;$B.height=9999;$W.bu‌​ffersize=$B;}'"
